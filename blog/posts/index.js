266ef90abd4e3e3302f8141a543e7a2e72cda836
papaya147 <asrivatsa6@gmail.com>
2021-10-15 17:53:49 +0530

    bus created

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,21 +9,22 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = {
-        id, title
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-    console.log('Listening on 4000');
-})
+  console.log("Listening on 4000");
+});
