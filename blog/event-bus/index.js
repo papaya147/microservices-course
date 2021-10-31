bbd5d773bceb5d12bebcc391b02f4000f84f16fc
papaya147 <asrivatsa6@gmail.com>
2021-10-31 18:39:38 +0530

    blog finished

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -4,8 +4,12 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log("4000: ", err.message);
@@ -16,8 +20,15 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log("4002: ", err.message);
   });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log("4003: ", err.message);
+  });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
